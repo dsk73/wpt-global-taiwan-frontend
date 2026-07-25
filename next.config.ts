6471dc5840642e23f90124c04026ef768de10d06
dsk73 <dhananjayskokate@gmail.com>
2026-07-25 23:01:58 +0530

    Updatedd next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "wpt-global-taiwan-backend.onrender.com",
+        pathname: "/uploads/**",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
         port: "1338",
