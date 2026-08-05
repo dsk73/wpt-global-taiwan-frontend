8d2ce79d052263fe79e268b6b98620a7e5e1a57c
Ubuntu <ubuntu@ip-172-31-39-122.ap-southeast-1.compute.internal>
2026-08-05 11:51:35 +0000

    Updated cnfiguaration

--- next.config.ts
+++ next.config.ts
@@ -3,25 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "54.179.185.61",
-        port: "1338",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1338",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "https",
-        hostname: "wpt-global-taiwan-backend.onrender.com",
-        pathname: "/uploads/**",
-      },
-    ],
-
+	  {
+	    protocol: "https",
+	    hostname: "api.wptglobal-asia.com",
+	    pathname: "/uploads/**",
+	  },
+	  {
+	    protocol: "http",
+	    hostname: "localhost",
+	    port: "1338",
+	    pathname: "/uploads/**",
+	  },
+	  {
+	    protocol: "https",
+	    hostname: "wpt-global-taiwan-backend.onrender.com",
+	    pathname: "/uploads/**",	
+	  },
+	],
     dangerouslyAllowLocalIP: true,
   },
 };
